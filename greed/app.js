52c4e077b4c29dbe1ce9e1fc25a935501442970d
BuianAlex <a.buyan@ukr.net>
2019-10-30 15:48:56 +0200

    ref to class

--- greed/app.js
+++ greed/app.js
@@ -1,60 +1,103 @@
-Array.prototype.last = function() {
-  return this[this.length - 1];
+Array.prototype.myPush = function(addData) {
+  return (this[this.length] = addData);
 };
 
-function Greed(rows, columns, startRow, startCol) {
-  this.coordinates = [[startRow, startCol]];
-  let lap = 1;
-  let cursor = [startRow, startCol];
+class Greed {
+  constructor(rows, columns, startRow, startCol) {
+    this.greedRows = rows;
+    this.greedColums = columns;
+    this.startRow = startRow;
+    this.startCol = startCol;
+    this.loop = 1;
+    this.coordinates = [[startRow, startCol]];
+    this.cursor = { row: startRow, col: startCol };
+    this.search();
+  }
 
-  this.moveR = () => {
-    let calcMove = startCol + lap;
-    for (let index = cursor[1] + 1; index <= calcMove; index++) {
-      if (index <= columns - 1 && cursor[0] >= 0) {
-        this.coordinates.push([cursor[0], index]);
+  moveRight() {
+    let calcMove = this.startCol + this.loop;
+    let strCursorPos = this.cursor.col + 1;
+    for (let index = strCursorPos; index <= calcMove; index += 1) {
+      if (
+        index <= this.greedColums - 1 &&
+        index >= 0 &&
+        this.cursor.row >= 0 &&
+        this.cursor.row <= this.greedRows - 1
+      ) {
+        this.coordinates.myPush([this.cursor.row, index]);
       }
     }
-    cursor[1] = calcMove;
-  };
+    this.cursor.col = calcMove;
+    return this;
+  }
 
-  this.moveB = () => {
-    let calcMove = startRow + lap;
-    for (let index = cursor[0] + 1; index <= calcMove; index++) {
-      if (index <= rows - 1 && index >= 0 && cursor[1] <= columns - 1) {
-        this.coordinates.push([index, cursor[1]]);
+  moveBottom() {
+    let calcMove = this.startRow + this.loop;
+    let strCursorPos = this.cursor.row + 1;
+    for (let index = strCursorPos; index <= calcMove; index += 1) {
+      if (
+        index <= this.greedRows - 1 &&
+        index >= 0 &&
+        this.cursor.col <= this.greedColums - 1 &&
+        this.cursor.col >= 0
+      ) {
+        this.coordinates.myPush([index, this.cursor.col]);
       }
     }
-    cursor[0] = calcMove;
-  };
+    this.cursor.row = calcMove;
+    return this;
+  }
 
-  this.moveL = () => {
-    let calcMove = startCol - lap;
-    for (let index = cursor[1] - 1; index >= calcMove; index--) {
-      if (index >= 0 && index <= columns - 1 && cursor[0] <= rows - 1) {
-        this.coordinates.push([cursor[0], index]);
+  moveLeft() {
+    let calcMove = this.startCol - this.loop;
+    let strCursorPos = this.cursor.col - 1;
+    for (let index = strCursorPos; index >= calcMove; index -= 1) {
+      if (
+        index <= this.greedColums - 1 &&
+        index >= 0 &&
+        this.cursor.row >= 0 &&
+        this.cursor.row <= this.greedRows - 1
+      ) {
+        this.coordinates.myPush([this.cursor.row, index]);
       }
     }
-    cursor[1] = calcMove;
-  };
-  this.moveT = () => {
-    let calcMove = startRow - lap;
-    for (let index = cursor[0] - 1; index >= calcMove; index--) {
-      if (index >= 0 && cursor[1] >= 0) {
-        this.coordinates.push([index, cursor[1]]);
+    this.cursor.col = calcMove;
+    return this;
+  }
+
+  moveTop() {
+    let calcMove = this.startRow - this.loop;
+    let strCursorPos = this.cursor.row - 1;
+    for (let index = strCursorPos; index >= calcMove; index -= 1) {
+      if (
+        index <= this.greedRows - 1 &&
+        index >= 0 &&
+        this.cursor.col <= this.greedColums - 1 &&
+        this.cursor.col >= 0
+      ) {
+        this.coordinates.myPush([index, this.cursor.col]);
       }
     }
-    cursor[0] = calcMove;
-    lap++;
-  };
+    this.cursor.row = calcMove;
+    this.loop += 1;
+    return this;
+  }
+
+  search() {
+    let maxItem = this.greedRows * this.greedColums;
+    while (this.coordinates.length < maxItem) {
+      this.moveRight();
+      this.moveBottom();
+      this.moveLeft();
+      this.moveTop();
+    }
+  }
+
+  printResult() {
+    console.log(this.coordinates);
+    console.log(this.coordinates.length);
+  }
 }
-const greed = new Greed(5, 6, 1, 4);
-let i = 0;
-while (i < 4) {
-  greed.moveR();
-  greed.moveB();
-  greed.moveL();
-  greed.moveT();
-  i++;
-}
 
-console.log(greed.coordinates.length);
+const greedQ = new Greed(5, 6, 1, 4);
+greedQ.printResult();
